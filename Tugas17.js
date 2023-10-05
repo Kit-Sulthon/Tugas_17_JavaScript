bf8bc79a8d3128cf54abee3f4bf2135219cd3521
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 15:48:24 +0700

    Tugas 17 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ Tugas17.js
@@ -0,0 +1,15 @@
+function Tugas17() {
+  var satu = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+  var dua =
+    "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+  console.log(satu.toLowerCase());
+  console.log(satu.toUpperCase());
+  /////////////////////////////////////
+  console.log(dua.toLowerCase());
+  console.log(dua.toUpperCase());
+  /////////////////////////////////////
+  console.log(String.fromCodePoint(76, 111, 114, 101, 109, 32));
+  console.log("76-111-114-101-109-32");
+}
+Tugas17();
